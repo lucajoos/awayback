216ea32f3566eaa0893474530739b0f35a4e7e3d
Luca Joos <70900757+lucajoos@users.noreply.github.com>
2021-01-27 22:01:34 +0100

    Add @license header, Add only function, Add isFired function

--- onceupon.js
+++ onceupon.js
@@ -1,3 +1,10 @@
+/**
+ * @license
+ * onceupon.js
+ * Released under MIT license
+ * Copyright Luca Joos
+ */
+
 module.exports = object => {
     let r = {
         events: {},
@@ -11,17 +18,15 @@ module.exports = object => {
                 if(data.length === 1) {
                     data = data[0];
                 }
-                
+
                 r.events[event].data.push(data);
             }
 
-            r.events[event].callbacks.forEach(e => {
-                if(e.type === 0 && e.fired === 0) {
-                    e.do(typeof data !== 'undefined' ? data : null, event);
-                    e.fired++;
-                } else if(e.type === 1) {
-                    e.do(typeof data !== 'undefined' ? data : null, event);
-                    e.fired++;
+            r.events[event].callbacks.forEach(c => {
+                if((c.type === 0 && c.fired === 0) || c.type === 1 || (c.type === 2 && c.fired === 0 && r.events[event].did === 0)) {
+                    c.fired++;
+                    r.events[event].did = r.events[event].did + 1;
+                    c.do(typeof data !== 'undefined' ? data : null, event);
                 }
             });
 
@@ -39,12 +44,10 @@ module.exports = object => {
                                 let exit = false;
 
                                 while(c.fired !== r.events[event].fired && !exit && !c.last) {
-                                    if(c.type === 0 && c.fired === 0) {
-                                        c.do(r.events[event].data[i] !== undefined ? r.events[event].data[c.fired] : null, event);
+                                    if((c.type === 0 && c.fired === 0) || c.type === 1 || (c.type === 2 && c.fired === 0 && r.events[event].did === 0)) {
                                         c.fired++;
-                                    } else if(c.type === 1) {
+                                        r.events[event].did = r.events[event].did + 1;
                                         c.do(r.events[event].data[i] !== undefined ? r.events[event].data[c.fired] : null, event);
-                                        c.fired++;
                                     } else {
                                         exit = true;
                                     }
@@ -57,6 +60,7 @@ module.exports = object => {
                 }),
 
                 fired: 0,
+                did: 0,
                 data: []
             }
         },
@@ -98,6 +102,29 @@ module.exports = object => {
                 )
             });
         },
+
+        only: (event, callback, last) => {
+            event.split('|').forEach(ce => {
+                ce = ce.trim();
+
+                if(r.events[ce] === undefined) {
+                    r.create(ce);
+                }
+
+                r.events[ce].callbacks.push(
+                    {
+                        do: callback,
+                        type: 2,
+                        fired: 0,
+                        last: typeof last === 'boolean' ? last : false
+                    }
+                )
+            });
+        },
+
+        isFired: event => {
+            return typeof r.events[event] === 'object' ? r.events[event].fired > 0 : false;
+        }
     };
 
     return typeof object === 'object' ? Object.assign(object, r) : r;
