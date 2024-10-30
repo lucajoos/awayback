9afddbae9d5e50282efd367ab9d7ed3c7f1044a9
Luca Raúl Joos <me@lucajoos.de>
2024-10-30 15:13:01 +0100

    fix: replaced lodash with lodash-es

--- awayback.ts
+++ awayback.ts
@@ -1,5 +1,5 @@
 import { Callback, CallbackType, Definition, Events, CallbackOptions, CallbackHandler } from './awayback.model.js'
-import { merge } from 'lodash'
+import { merge } from 'lodash-es'
 
 /**
  * @license
